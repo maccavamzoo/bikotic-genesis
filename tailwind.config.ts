d4a7ec863c9f0b19faae23eb2bdc8b18b83b6520
maccavamzoo <44295880+maccavamzoo@users.noreply.github.com>
2025-12-19 14:54:32 +0000

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'bikotic-blue': '#4a4b88',
+        'bikotic-blue': '#0891b2',
         'bikotic-blue-dark': '#3a3b6b',
       },
       typography: {
