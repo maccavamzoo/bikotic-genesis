79d5410c14c28cce45b9676eeff77950c4f8f30d
maccavamzoo <44295880+maccavamzoo@users.noreply.github.com>
2026-01-03 17:45:50 +0000

    Update GiantDefyTcrPropelChartWrapper.tsx

--- app/components/GiantDefyTcrPropelChartWrapper.tsx
+++ app/components/GiantDefyTcrPropelChartWrapper.tsx
@@ -4,18 +4,7 @@ import dynamic from 'next/dynamic'
 
 const GiantDefyTcrPropelChart = dynamic(
   () => import('./GiantDefyTcrPropelChart'),
-  { 
-    ssr: false,
-    loading: () => (
-      <div className="w-full bg-white rounded-xl shadow-lg p-8 my-8">
-        <div className="bg-gray-50 rounded-xl p-6">
-          <div className="flex items-center justify-center h-[400px] md:h-[600px]">
-            <div className="text-gray-400">Loading chart...</div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  { ssr: false }
 )
 
 export default GiantDefyTcrPropelChart
