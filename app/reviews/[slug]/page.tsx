662e6726a0102dc757d68902b06ffe5a65bd3922
maccavamzoo <44295880+maccavamzoo@users.noreply.github.com>
2026-01-19 17:38:07 +0000

    Update page.tsx

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -75,7 +75,7 @@ export default async function ReviewPage({
           <iframe 
             width="100%" 
             height="100%" 
-            src={`https://www.youtube.com/embed/${youtubeId}`}
+            src={`https://www.youtube-nocookie.com/embed/${youtubeId}`}
             title="YouTube video player" 
             frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
